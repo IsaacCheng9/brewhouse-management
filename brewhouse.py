e71396e7538184e9bd119f1ddcd4cd6f24ed17e8
Your Name <you@example.com>
2019-12-04 01:32:42 +0000

    Added inventory management

--- brewhouse.py
+++ brewhouse.py
@@ -3,12 +3,14 @@ A system to predict demand and help monitor and schedule brewing processes for
 Barnaby's Brewhouse.
 """
 
+import json
 import sys
 from datetime import datetime
 from typing import Tuple
 
 import pandas
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QIntValidator
 from PyQt5.QtWidgets import QDialog, QMainWindow
 
 from brewhouse_setup import Ui_mwindow_brewhouse
@@ -190,6 +192,7 @@ class BrewhouseWindow(QMainWindow, Ui_mwindow_brewhouse):
               "%\nPilsner Growth: " + str(pilsner_growth_pct) +
               "%\nDunkel Growth: " + str(dunkel_growth_pct) + "%")
 
+        # Displays percentage of sales provided by each beer.
         self.lbl_red_helles_growth.setText(
             "Organic Red Helles - " + str(red_helles_growth_pct) + "%")
         self.lbl_pilsner_growth.setText(
@@ -230,7 +233,7 @@ class BrewhouseWindow(QMainWindow, Ui_mwindow_brewhouse):
         month_difference = ((prediction_date.year - final_date.year) *
                             12 + (prediction_date.month - final_date.month))
 
-        # Predicts beer sales for that month, rouned down to nearest integer.
+        # Predicts beer sales for that month, rounded down to nearest integer.
         last_month_filter = data_frame["Date Required"].str.contains("Oct-19")
         beer_filter = data_frame["Recipe"].str.contains(prediction_beer)
         last_month_data = data_frame[last_month_filter & beer_filter]
@@ -245,24 +248,143 @@ class BrewhouseWindow(QMainWindow, Ui_mwindow_brewhouse):
         elif prediction_beer == "Organic Dunkel":
             prediction_sales = int((last_month_sales *
                                     (dunkel_growth ** month_difference)))
-        print("\nEstimated Number of Sales: " + str(prediction_sales))
+        print("\nEstimated Number of Monthly Sales: " + str(prediction_sales))
 
+        # Shows the sales prediction for the given beer and date.
         self.lbl_predict_result.setText(
-            "Estimated Number of Sales: " + str(prediction_sales))
+            "Estimated Number of Monthly Sales: " + str(prediction_sales))
 
 
 class InventoryManagementDialog(QDialog, Ui_dialog_inv_management):
     """Contains the dialog window for inventory management."""
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        # Restricts volume input to only numbers.
+        self.only_int = QIntValidator()
+        self.line_edit_update_inv_volume.setValidator(self.only_int)
+
+        # Reads the inventory and gets volume for each beer.
+        (inventory_dict, red_helles_volume, pilsner_volume,
+         dunkel_volume) = self.read_inventory()
+
+        # Connects 'Add to Inventory' button to add inventory volume.
+        self.btn_add_inv.clicked.connect(lambda: self.add_inventory(
+            inventory_dict, red_helles_volume, pilsner_volume, dunkel_volume))
+        # Connects 'Remove from Inventory' button to remove inventory volume.
+        self.btn_remove_inv.clicked.connect(lambda: self.remove_inventory(
+            inventory_dict, red_helles_volume, pilsner_volume, dunkel_volume))
+
+        # Updates the volumes in the inventory in the UI.
+        self.update_inventory()
+
+    def read_inventory(self) -> Tuple[dict, int, int, int]:
+        """Reads the inventory and gets volume for each beer.
+
+        Returns:
+            inventory_dict (dict): Dictionary storing the volume for each beer.
+            red_helles_volume (int): Volume (L) of Red Helles.
+            pilsner_volume (int): Volume (L) of Pilsner.
+            dunkel_volume (int): Volume (L) of Dunkel.
+        """
+        with open("inventory.json", "r") as inventory_file:
+            try:
+                inventory_dict = json.load(inventory_file)
+                red_helles_volume = int(
+                    inventory_dict["Organic Red Helles"]["Volume"])
+                pilsner_volume = int(
+                    inventory_dict["Organic Pilsner"]["Volume"])
+                dunkel_volume = int(inventory_dict["Organic Dunkel"]["Volume"])
+            except ValueError:
+                print("Empty JSON file.")
+
+        return inventory_dict, red_helles_volume, pilsner_volume, dunkel_volume
+
+    def update_inventory(self):
+        """Updates the volumes in the inventory in the UI."""
+        (inventory_dict, red_helles_volume, pilsner_volume,
+         dunkel_volume) = self.read_inventory()
+
+        # Shows the volume and number of bottles of Red Helles.
+        red_helles_quantity = int(red_helles_volume / 500)
+        self.lbl_red_helles_inv.setText(
+            "Organic Red Helles - " + str(red_helles_volume) + " L / " +
+            str(red_helles_quantity) + " bottle(s)")
+
+        # Shows the volume and number of bottles of Pilsner.
+        pilsner_quantity = int(pilsner_volume / 500)
+        self.lbl_pilsner_inv.setText(
+            "Organic Pilsner - " + str(pilsner_volume) + " L / " +
+            str(pilsner_quantity) + " bottle(s)")
+
+        # Shows the volume and number of bottles of Dunkel.
+        dunkel_quantity = int(dunkel_volume / 500)
+        self.lbl_dunkel_inv.setText(
+            "Organic Dunkel - " + str(dunkel_volume) + " L / " +
+            str(dunkel_quantity) + " bottle(s)")
+
+    def save_inventory(self, inventory_dict: dict):
+        """Saves the new inventory to the JSON file.
+
+        Args:
+            inventory_dict (dict): Dictionary storing the volume for each beer.
+        """
+        with open("inventory.json", "w") as outfile:
+            json.dump(inventory_dict, outfile, ensure_ascii=False, indent=4)
+
+    def add_inventory(self, inventory_dict: dict, red_helles_volume: int,
+                      pilsner_volume: int, dunkel_volume: int):
+        """Adds the given volume to the given beer in the inventory.
+
+        Args:
+            inventory_dict (dict): Dictionary storing the volume for each beer.
+            red_helles_volume (int): Volume (L) of Red Helles.
+            pilsner_volume (int): Volume (L) of Pilsner.
+            dunkel_volume (int): Volume (L) of Dunkel.
+        """
+        # Adds inputted volume to the selected beer.
+        add_beer = self.combo_box_update_inv_recipe.currentText()
+        add_volume = self.line_edit_update_inv_volume.text()
+        inventory_dict[add_beer]["Volume"] += int(add_volume)
+
+        # Saves the new inventory to the JSON file.
+        self.save_inventory(inventory_dict)
+        # Updates the volumes in the inventory in the UI.
+        self.update_inventory()
+
+    def remove_inventory(self, inventory_dict: dict, red_helles_volume: int,
+                         pilsner_volume: int, dunkel_volume: int):
+        """Removes the given volume to the given beer in the inventory.
+
+        Args:
+            inventory_dict (dict): Dictionary storing the volume for each beer.
+            red_helles_volume (int): Volume (L) of Red Helles.
+            pilsner_volume (int): Volume (L) of Pilsner.
+            dunkel_volume (int): Volume (L) of Dunkel.
+        """
+        # Adds inputted volume to the selected beer.
+        remove_beer = self.combo_box_update_inv_recipe.currentText()
+        remove_volume = self.line_edit_update_inv_volume.text()
+        inventory_dict[remove_beer]["Volume"] -= int(remove_volume)
+
+        # Saves the new inventory to the JSON file.
+        self.save_inventory(inventory_dict)
+        # Updates the volumes in the inventory in the UI.
+        self.update_inventory()
 
 
 class ProcessMonitoringDialog(QDialog, Ui_dialog_monitoring):
     """Contains the dialog window for process monitoring."""
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        # Restricts volume input to only numbers.
+        self.only_int = QIntValidator()
+        self.line_edit_new_volume.setValidator(self.only_int)
 
 
 # Prevents the code from executing when the script is imported as a module.
