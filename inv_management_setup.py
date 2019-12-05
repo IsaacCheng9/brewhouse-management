db888e479707223c383f8aae2b96ea1716ee784c
Isaac <isaaccheng9@gmail.com>
2019-12-05 15:51:39 +0000

    Auto PEP8ed most line length problems

--- inv_management_setup.py
+++ inv_management_setup.py
@@ -30,14 +30,19 @@ class Ui_dialog_inv_management(object):
         self.scroll_area.setWidgetResizable(True)
         self.scroll_area.setObjectName("scroll_area")
         self.scroll_area_widget_contents = QtWidgets.QWidget()
-        self.scroll_area_widget_contents.setGeometry(QtCore.QRect(0, 0, 478, 353))
-        self.scroll_area_widget_contents.setObjectName("scroll_area_widget_contents")
-        self.verticalLayoutWidget = QtWidgets.QWidget(self.scroll_area_widget_contents)
+        self.scroll_area_widget_contents.setGeometry(
+            QtCore.QRect(0, 0, 478, 353))
+        self.scroll_area_widget_contents.setObjectName(
+            "scroll_area_widget_contents")
+        self.verticalLayoutWidget = QtWidgets.QWidget(
+            self.scroll_area_widget_contents)
         self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 471, 330))
         self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
-        self.vert_layout_inv_management = QtWidgets.QVBoxLayout(self.verticalLayoutWidget)
+        self.vert_layout_inv_management = QtWidgets.QVBoxLayout(
+            self.verticalLayoutWidget)
         self.vert_layout_inv_management.setContentsMargins(0, 0, 0, 0)
-        self.vert_layout_inv_management.setObjectName("vert_layout_inv_management")
+        self.vert_layout_inv_management.setObjectName(
+            "vert_layout_inv_management")
         self.lbl_inv_management = QtWidgets.QLabel(self.verticalLayoutWidget)
         font = QtGui.QFont()
         font.setFamily("Arial")
@@ -81,22 +86,32 @@ class Ui_dialog_inv_management(object):
         self.vert_layout_inv_management.addWidget(self.lbl_update_inv)
         self.grid_layout_update_inv = QtWidgets.QGridLayout()
         self.grid_layout_update_inv.setObjectName("grid_layout_update_inv")
-        self.lbl_update_inv_volume = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.lbl_update_inv_volume = QtWidgets.QLabel(
+            self.verticalLayoutWidget)
         self.lbl_update_inv_volume.setObjectName("lbl_update_inv_volume")
-        self.grid_layout_update_inv.addWidget(self.lbl_update_inv_volume, 2, 0, 1, 1)
-        self.lbl_update_inv_recipe = QtWidgets.QLabel(self.verticalLayoutWidget)
+        self.grid_layout_update_inv.addWidget(
+            self.lbl_update_inv_volume, 2, 0, 1, 1)
+        self.lbl_update_inv_recipe = QtWidgets.QLabel(
+            self.verticalLayoutWidget)
         self.lbl_update_inv_recipe.setObjectName("lbl_update_inv_recipe")
-        self.grid_layout_update_inv.addWidget(self.lbl_update_inv_recipe, 1, 0, 1, 1)
-        self.combo_box_update_inv_recipe = QtWidgets.QComboBox(self.verticalLayoutWidget)
-        self.combo_box_update_inv_recipe.setObjectName("combo_box_update_inv_recipe")
+        self.grid_layout_update_inv.addWidget(
+            self.lbl_update_inv_recipe, 1, 0, 1, 1)
+        self.combo_box_update_inv_recipe = QtWidgets.QComboBox(
+            self.verticalLayoutWidget)
+        self.combo_box_update_inv_recipe.setObjectName(
+            "combo_box_update_inv_recipe")
         self.combo_box_update_inv_recipe.addItem("")
         self.combo_box_update_inv_recipe.addItem("")
         self.combo_box_update_inv_recipe.addItem("")
-        self.grid_layout_update_inv.addWidget(self.combo_box_update_inv_recipe, 1, 1, 1, 1)
-        self.line_edit_update_inv_volume = QtWidgets.QLineEdit(self.verticalLayoutWidget)
+        self.grid_layout_update_inv.addWidget(
+            self.combo_box_update_inv_recipe, 1, 1, 1, 1)
+        self.line_edit_update_inv_volume = QtWidgets.QLineEdit(
+            self.verticalLayoutWidget)
         self.line_edit_update_inv_volume.setMaxLength(13)
-        self.line_edit_update_inv_volume.setObjectName("line_edit_update_inv_volume")
-        self.grid_layout_update_inv.addWidget(self.line_edit_update_inv_volume, 2, 1, 1, 1)
+        self.line_edit_update_inv_volume.setObjectName(
+            "line_edit_update_inv_volume")
+        self.grid_layout_update_inv.addWidget(
+            self.line_edit_update_inv_volume, 2, 1, 1, 1)
         self.vert_layout_inv_management.addLayout(self.grid_layout_update_inv)
         self.hori_layout_update_inv = QtWidgets.QHBoxLayout()
         self.hori_layout_update_inv.setObjectName("hori_layout_update_inv")
@@ -106,10 +121,12 @@ class Ui_dialog_inv_management(object):
         self.btn_remove_inv = QtWidgets.QPushButton(self.verticalLayoutWidget)
         self.btn_remove_inv.setObjectName("btn_remove_inv")
         self.hori_layout_update_inv.addWidget(self.btn_remove_inv)
-        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        spacerItem = QtWidgets.QSpacerItem(
+            40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
         self.hori_layout_update_inv.addItem(spacerItem)
         self.vert_layout_inv_management.addLayout(self.hori_layout_update_inv)
-        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        spacerItem1 = QtWidgets.QSpacerItem(
+            20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.vert_layout_inv_management.addItem(spacerItem1)
         self.scroll_area.setWidget(self.scroll_area_widget_contents)
         self.gridLayout.addWidget(self.scroll_area, 0, 0, 1, 1)
@@ -119,20 +136,33 @@ class Ui_dialog_inv_management(object):
 
     def retranslateUi(self, dialog_inv_management):
         _translate = QtCore.QCoreApplication.translate
-        dialog_inv_management.setWindowTitle(_translate("dialog_inv_management", "Inventory Management - Barnaby\'s Brewhouse"))
-        self.lbl_inv_management.setText(_translate("dialog_inv_management", "Inventory Management"))
+        dialog_inv_management.setWindowTitle(_translate(
+            "dialog_inv_management", "Inventory Management - Barnaby\'s Brewhouse"))
+        self.lbl_inv_management.setText(_translate(
+            "dialog_inv_management", "Inventory Management"))
         self.lbl_inv.setText(_translate("dialog_inv_management", "Inventory"))
-        self.lbl_red_helles_inv.setText(_translate("dialog_inv_management", "Organic Red Helles - "))
-        self.lbl_pilsner_inv.setText(_translate("dialog_inv_management", "Organic Pilsner - "))
-        self.lbl_dunkel_inv.setText(_translate("dialog_inv_management", "Organic Dunkel - "))
-        self.lbl_update_inv.setText(_translate("dialog_inv_management", "Update Inventory"))
-        self.lbl_update_inv_volume.setText(_translate("dialog_inv_management", "Volume (L):"))
-        self.lbl_update_inv_recipe.setText(_translate("dialog_inv_management", "Beer Recipe:"))
-        self.combo_box_update_inv_recipe.setItemText(0, _translate("dialog_inv_management", "Organic Red Helles"))
-        self.combo_box_update_inv_recipe.setItemText(1, _translate("dialog_inv_management", "Organic Pilsner"))
-        self.combo_box_update_inv_recipe.setItemText(2, _translate("dialog_inv_management", "Organic Dunkel"))
-        self.btn_add_inv.setText(_translate("dialog_inv_management", "Add to Inventory"))
-        self.btn_remove_inv.setText(_translate("dialog_inv_management", "Remove from Inventory"))
+        self.lbl_red_helles_inv.setText(_translate(
+            "dialog_inv_management", "Organic Red Helles - "))
+        self.lbl_pilsner_inv.setText(_translate(
+            "dialog_inv_management", "Organic Pilsner - "))
+        self.lbl_dunkel_inv.setText(_translate(
+            "dialog_inv_management", "Organic Dunkel - "))
+        self.lbl_update_inv.setText(_translate(
+            "dialog_inv_management", "Update Inventory"))
+        self.lbl_update_inv_volume.setText(
+            _translate("dialog_inv_management", "Volume (L):"))
+        self.lbl_update_inv_recipe.setText(
+            _translate("dialog_inv_management", "Beer Recipe:"))
+        self.combo_box_update_inv_recipe.setItemText(
+            0, _translate("dialog_inv_management", "Organic Red Helles"))
+        self.combo_box_update_inv_recipe.setItemText(
+            1, _translate("dialog_inv_management", "Organic Pilsner"))
+        self.combo_box_update_inv_recipe.setItemText(
+            2, _translate("dialog_inv_management", "Organic Dunkel"))
+        self.btn_add_inv.setText(_translate(
+            "dialog_inv_management", "Add to Inventory"))
+        self.btn_remove_inv.setText(_translate(
+            "dialog_inv_management", "Remove from Inventory"))
 
 
 if __name__ == "__main__":
